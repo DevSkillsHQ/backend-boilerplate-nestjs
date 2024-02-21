b1dc9478642d777d3678e95fe12ca889bc121131
ehsan-alva <155959659+ehsan-alva@users.noreply.github.com>
2024-02-21 15:01:17 +0100

    feat: add dependabot (#5)
    * feat: add dependabot
    
    * fix: wait till api port is open
    
    * fix: race condition handling
    
    * fix: localhost to 127.0.0.1
    
    * fix: timeout to 300ms
    
    * fix: extra step removed
    
    * fix: log added
    
    * fix: rollback changes
    
    * fix: race condition
    
    * fix: 10s timeout added
    
    * fix: reading the port from cypress.json
    
    * fix: replace npm run test with cypress
    
    * fix: timeout increased to 30s
    
    * fix: rollback cypress
    
    * fix: cypress v6
    
    * fix: absolute address added to wait-on
    
    * fix: wait-on ip
    
    * fix: listen on all interface
    
    * fix: cypress wait-on
    
    * fix: wait-on /ping
    
    * fix: wait-on option added to cypress
    
    * fix: removed 0.0.0.0
    
    * fix: dependencies added to auto-merge.yml file
    
    * fix: ruby added to auto-merge.yml file
    
    * fix: extract port from base url if api url is not provided
    
    * fix: wait-on timout loaded from variables and listen to port instead of url
    
    * fix: cypress updated to 12.17.1
    
    * fix: cypress config updated
    
    * fix: step added to run test if api url is set
    
    * fix: write permission added to the workflow and automerge will be triggered on patch update
    
    * fix: automerge will be triggered on all update
    
    ---------
    
    Co-authored-by: Ehsan Mirzarazi <ehsanmirzarazi@ehsans-mbp.home>
    Co-authored-by: Ludvig Persson <ludvig.persson@alvalabs.io>
    Co-authored-by: Ehsan Mirzarazi <ehsanmirzarazi@Ehsans-MBP.lan>

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  env: {
+    apiUrl: 'http://localhost:3000',
+  },
+  e2e: {
+    supportFile: false,
+    setupNodeEvents(on, config) {},
+  },
+})
